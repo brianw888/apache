97de6db9c61372dc0a574e1becee0c788aeeb2da
ChefDK User <chef@ip-172-31-13-83.us-west-1.compute.internal>
2015-06-02 23:23:24 +0000

    new files added

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,23 @@ require 'spec_helper'
 describe 'apache::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe package('apache2') do
+    it { should be_installed }
+  end
+
+  describe file('/var/www/html/index.html') do
+    it { should be_file }
+  end
+
+  describe command('curl localhost') do
+    its(:stdout) { should match /Hello, World!/ }
+  end
+
+  describe service('apache2') do
+    it { should be_running }
+  end
+
+  describe port(80) do
+    it { should be_listening }
   end
 end
